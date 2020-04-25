3f10bd7d3efda5abdd8cf181f38dc3175ca61fda
code-fi <codewithfrank@gmail.com>
2020-04-25 10:57:22 +0000

    patches

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,42 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import TopNavigation from '../../components/top-menu';
-import { getHeadLines } from '../../services/http';
-import NewsHero from '../../components/news-hero';
-import {shuffle} from 'lodash';
-import Loader from '../../components/loader';
+import React from 'react';
 
 export default function Home() {
-    const [headlines, setHeadlines] = useState([]);
-    const [page,setPage] = useState(null);
-    const [appState, setAppState] = useState({
-        loading: true,
-        error: false
-    });
 
-
-    useEffect(() => {
-        setAppState(a => ({ ...a, loading: true }));
-        getHeadLines(page)
-        .then(response => {
-            if (response && response.data && response.data.status === 'ok') {
-                setHeadlines(shuffle(response.data.articles));
-            }
-        }, error => {
-            console.log(error);
-            setAppState(a => ({ ...a, error: true }));
-        })
-        .finally(_ => {
-            setAppState(a => ({ ...a, loading: false }));
-        });
-    }, [page]);
-
-
-    const ContentView = appState.loading ? <Loader /> : <NewsHero headlines={headlines} />;
     return (
 
         <div className="content-wrapper">
-            <TopNavigation onChangePage={(page)=>setPage(page)} />
-            {ContentView}
+            <h2 style={{ color: "#b92c2c", fontSize: 48, position: "relative", top: "50%", left: "40%" }}>Learning React Hooks</h2>
         </div>
 
     )
