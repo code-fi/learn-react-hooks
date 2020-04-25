97f0ab8326b9c51dae215f67a980ee7375ce6ab0
code-fi <codewithfrank@gmail.com>
2020-04-25 10:59:30 +0000

    removed api keys

--- src/services/http.js
+++ src/services/http.js
@@ -5,7 +5,7 @@ const source = axios.CancelToken.source();
 const instance = axios.create({
     baseURL: 'https://newsapi.org/v2/',
     timeout: 30000,
-    headers: { 'Authorization': `Bearer 7be5a33db897460498d807cf31d0709c` },
+    headers: { 'Authorization': `Bearer __API_KEY__` },
     cancelToken: source.token
 });
 
